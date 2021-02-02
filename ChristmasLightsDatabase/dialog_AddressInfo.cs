7b1f51be1107afc831bca87fcf58104e68aa796c
Keaton Dalquist <dalquist.keaton221@gmail.com>
2021-02-01 20:33:01 -0600

    Set up basic dialog fragment when clicking on a row in the list view. Will show more information on the house and linke to google maps in the future

--- ChristmasLightsDatabase/dialog_AddressInfo.cs
+++ ChristmasLightsDatabase/dialog_AddressInfo.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
 using Android.App;
 using Android.Content;
 using Android.OS;
@@ -14,24 +13,30 @@ namespace ChristmasLightsDatabase
 {
     class dialog_AddressInfo : DialogFragment
     {
-        private TextView myListView;
-        private TextView myListView;
-        private TextView myListView;
-        private TextView myListView;
-        private TextView myListView;
-        private TextView myListView;
-        private TextView myListView;
-        private TextView myListView;
-        List<addressHolder> myItems;
+        private TextView AddressLineValue;
+        private TextView CityValue;
+        private TextView StateValue;
+        private TextView ZipCodeValue;
+        string addressLine, city, state, zipCode;
         public dialog_AddressInfo(List<addressHolder> mItems, int position)
         {
-            myItems = mItems;
-            //note to self, review this later: https://camposha.info/xamarin-dialog-fragment-with-simple-listview/
+            addressLine = mItems[position].addressLine;
+            city = mItems[position].city;
+            state = mItems[position].state;
+            zipCode = mItems[position].zipCode;
         }
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             base.OnCreateView(inflater, container, savedInstanceState);
             var view = inflater.Inflate(Resource.Layout.dialog_AddressInfo, container, false);
+            AddressLineValue = view.FindViewById<TextView>(Resource.Id.addressLineValue);
+            AddressLineValue.Text = addressLine;
+            CityValue = view.FindViewById<TextView>(Resource.Id.cityValue);
+            CityValue.Text = city;
+            StateValue = view.FindViewById<TextView>(Resource.Id.stateValue);
+            StateValue.Text = state;
+            ZipCodeValue = view.FindViewById<TextView>(Resource.Id.zipValue);
+            ZipCodeValue.Text = zipCode;
             return view;
         }
     }
